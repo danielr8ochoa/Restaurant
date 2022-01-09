c08b4ed6c4a70b5d63a3c1bbbeafe9afe9fadb97
Daniel Ochoa <daniel.romero48@uabc.edu.mx>
2022-01-08 19:51:16 -0800

    Eliminando productos si la cantidad es CERO

--- js/app.js
+++ js/app.js
@@ -144,7 +144,9 @@ function agregarPlatillo(producto) {
 
 
     } else {
-        console.log('No es mayor a 0');
+        // Eliminar elementos cuando la cantidad es 0
+        const resultado = pedido.filter( articulo => articulo.id !== producto.id);
+        cliente.pedido = [...resultado];
     }
     console.log(cliente.pedido);
 }
